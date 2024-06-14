9a9c87656cdc0032e5d9654ffd49ab0313f71ceb
SleekMind <163165737+SleekMind@users.noreply.github.com>
2024-06-14 10:40:33 +0530

    Add files via upload

---
+++ D_Kth_Excluded.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long
+#define endl "\n"
+
+void vinayak()
+{
+    int n, q;
+    cin >> n >> q;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    sort(v.begin(), v.end());
+    while (q--)
+    {
+        int val;
+        cin >> val;
+        int lo = 0;
+        // whenever range is given 1e18 keep it INT_64MAX
+        // but make sure to keep value such that it doesn't overflow anywhere.. 
+        int hi = INT_FAST64_MAX;
+        int ans = 0;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+
+            int k = lower_bound(v.begin(), v.end(), mid) - v.begin();
+            if (k < n && v[k] == mid)
+                k++;
+            if (mid - k >= val)
+            {
+                ans = mid;
+                hi = mid - 1;
+            }
+            else
+            {
+                lo = mid + 1;
+            }
+        }
+        cout << ans << endl;
+    }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false); // don't use while debugging
+    cin.tie(NULL);                    // don't use in interactive problems
+    cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        vinayak();
+    return 0;
+}
